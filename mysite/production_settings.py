6376aebcb185532aef16f8715db997d3c8e595fe
Dhruv <131877805+getnewone@users.noreply.github.com>
2025-05-17 01:00:09 +1000

    Fix: Remove headers from send_mail in views and update STATICFILES_DIRS

--- mysite/production_settings.py
+++ mysite/production_settings.py
@@ -68,7 +68,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files configuration (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [] # Updated to remove os.path.join(BASE_DIR, 'static')
 
 # Logging
 LOGGING = {
