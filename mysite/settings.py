7d3fd2bb391defecf321bf477309424c07f98ebb
Dhruv <131877805+getnewone@users.noreply.github.com>
2025-05-16 20:44:17 +1000

    updated SQL path

--- mysite/settings.py
+++ mysite/settings.py
@@ -203,10 +203,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+# Add/modify this in your production_settings.py
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/opt/render/project/src/', 'db.sqlite3'),
     }
 }
 
